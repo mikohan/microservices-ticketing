697eee8610d921d2f00579e09e5d4a4676a1b0a2
mikohan <angara99@gmail.com>
2021-09-15 17:26:25 +0300

    Added middlewares

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routs/current-user';
+import { signinRouter } from './routs/signin';
+import { signupRouter } from './routs/signup';
+import { signoutRouter } from './routs/signout';
 
 const app = express();
 
 app.use(json());
 app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
 
 app.listen(3000, () => {
   console.log('Listening on 3000!');
