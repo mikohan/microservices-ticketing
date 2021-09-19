ef2faacce49a08165abc1a77f03c608f48bbce05
mikohan <angara99@gmail.com>
2021-09-19 15:12:23 +0300

    Added create user logic

--- auth/src/routs/signup.ts
+++ auth/src/routs/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
 
@@ -21,9 +22,19 @@ router.post(
       throw new RequestValidationError(errors.array());
     }
     const { email, password } = req.body;
-    console.log('Creating a user ...', email, password);
-    throw new DatabaseConnectionError();
-    res.send({});
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      console.log('User in use');
+      return res.send({});
+    }
+
+    const user = User.build({
+      email,
+      password,
+    });
+    await user.save();
+    res.status(201).send(user);
   }
 );
 
