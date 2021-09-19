1b6051e81918794b283a4ae3996cd4cfbdafe7c7
mikohan <angara99@gmail.com>
2021-09-19 12:27:36 +0300

    Implemented abstrach class custom error

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,14 +1,16 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from './custom-error';
+
+export class DatabaseConnectionError extends CustomError {
   statusCode = 500;
   reason = 'Error connecting to database';
   constructor() {
-    super();
+    super('Error connecting to database');
     //Oly because we are extending a built in class
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
 
   serializeErrors() {
-    return [{ mesage: this.reason }];
+    return [{ message: this.reason }];
   }
 }
