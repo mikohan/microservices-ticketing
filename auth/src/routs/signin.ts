afffdd629f639ee004b6499a4910015cbee199d1
mikohan <angara99@gmail.com>
2021-10-05 15:40:35 +0300

    Added signin logic errors

--- auth/src/routs/signin.ts
+++ auth/src/routs/signin.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/singnin', (req, res) => {
-  console.log('Hi there');
-});
+router.post(
+  '/api/users/singnin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .notEmpty()
+      .trim()
+      .withMessage('You must supply a password'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+  }
+);
 
 export { router as signinRouter };
