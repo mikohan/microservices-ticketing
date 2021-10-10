7841651acff4ed5910939c34d05bb9abce0597db
mikohan <angara99@gmail.com>
2021-10-10 17:31:32 +0300

    Added tiest for ticketing new routs

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,8 +5,11 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined in kubernetis secure pod');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined as env in pod');
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {});
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log('Connected to mongoDB');
   } catch (e) {
     console.log(e);
