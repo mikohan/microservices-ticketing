dcd1e9e6f2ac4e3b64fe2b5927e2a83167b5b2c3
mikohan <angara99@gmail.com>
2021-10-10 18:50:08 +0300

    All test are passed for now

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@angara_digital/common';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@angara_digital/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than zerro'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createTicketRouter };
