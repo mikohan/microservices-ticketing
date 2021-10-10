5047b80a47886298fb6197d482b568bb7ee93e42
mikohan <angara99@gmail.com>
2021-10-10 17:48:46 +0300

    Added route for new ticket

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,6 +3,8 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
+import { createTicketRouter } from './routes/new';
+
 import { errorHandler, NotFoundError } from '@angara_digital/common';
 // Comment
 
@@ -16,6 +18,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- tickets/src/routes/__tests__/new.test.ts
+++ tickets/src/routes/__tests__/new.test.ts
@@ -1,7 +1,11 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-it('has a router handler listening on /app/tickets for post request', async () => {});
+it('has a router handler listening on /app/tickets for post request', async () => {
+  const response = await request(app).post('/app/tickets').send({});
+
+  expect(response.status).not.toEqual(404);
+});
 it('can only assessed if the user is signed in', async () => {});
 it('returns an error if an invalid title is provided', async () => {});
 it('returns an error if an invalid price is provided', async () => {});
