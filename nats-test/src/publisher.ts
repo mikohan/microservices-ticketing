3580d54bb81256748b5fa9ba4677744faafa41f5
mikohan <angara99@gmail.com>
2021-10-12 16:53:21 +0300

    Sick day

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,9 +1,23 @@
 import nats from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+// added comment to not forget to publish
 
-const stan = nats.connect('ticketing', 'abc', {
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Publisher connected to nats');
+
+  const data = JSON.stringify({
+    id: '123',
+    title: 'my ticket',
+    price: 20,
+  });
+
+  stan.publish('ticket:created', data, () => {
+    console.log('Event published');
+  });
 });
