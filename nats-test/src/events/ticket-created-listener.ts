b502910fe521b161123a94107f393846c13e3454
mikohan <angara99@gmail.com>
2022-01-11 16:03:39 +0300

    Added typing to listenre

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,15 +1,15 @@
-import nats from "node-nats-streaming";
-import { Listener } from "./base-listener";
-import { TicketCreatedEvent } from "./ticket-created-event";
-import { Subjects } from "./subjects";
+import nats from 'node-nats-streaming';
+import { Listener } from './base-listener';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
 
-  queueGroupName: string = "payments-service";
+  queueGroupName: string = 'payments-service';
 
-  onMessage(data: TicketCreatedEvent["data"], msg: nats.Message): void {
-    console.log("Event data!", data);
+  onMessage(data: TicketCreatedEvent['data'], msg: nats.Message): void {
+    console.log('Event data!', data);
     msg.ack();
   }
 }
