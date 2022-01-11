5c498cf21346cba6c7bcd0a77eecb9743a18c396
mikohan <angara99@gmail.com>
2022-01-11 14:51:10 +0300

    Finished with abstract class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -30,8 +30,11 @@ process.on('SIGTERM', ()=> stan.close());
 abstract class Listener {
   abstract subject: string;
   abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message) : void;
   private client: Stan;
   protected ackWait = 5 * 1000;
+
+
 
   constructor(client: Stan){
     this.client = client;
@@ -54,10 +57,14 @@ abstract class Listener {
       console.log(
         `Message received: ${this.subject} / ${this.queueGroupName}`
       )
+      const parsedData = this.parseMessage(msg)
+      this.onMessage(parsedData, msg)
     })
   }
   parseMessage(msg: Message) {
     const data = msg.getData();
+    return typeof data === 'string' ?
+    JSON.parse(data): JSON.parse(data.toString('utf8'))
   }
 
 }
