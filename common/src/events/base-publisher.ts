76534cc14e4f89fa060ee37d8d45d1e235ba2447
mikohan <angara99@gmail.com>
2022-01-14 16:03:00 +0300

    Updates

--- common/src/events/base-publisher.ts
+++ common/src/events/base-publisher.ts
@@ -15,6 +15,7 @@ export abstract class Publisher<T extends Event> {
   }
 
   publish(data: T['data']): Promise<void> {
+    /*publish function*/
     return new Promise((resolve, reject) => {
       this.client.publish(this.subject, JSON.stringify(data), (err) => {
         if (err) {
